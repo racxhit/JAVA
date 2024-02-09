9d967ac657ba50ce95d6a105c03f4d1221f968c7
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-09 19:51:25 +0530

    Update P4.java

--- Task 1/P4.java
+++ Task 1/P4.java
@@ -1,10 +1,27 @@
+/*
+Using the switch statement, write a menu-driven program to calculate the maturity amount of a bank deposit.
+The user is given the following options: 
+
+(i) Term Deposit
+(ii) Recurring Deposit
+
+For option (i) accept Principal (p), rate of interest (r) and time period in years (n). 
+Calculate and output the maturity amount (a) receivable using the formula a = p[1 + r / 100]n.
+For option (ii) accept monthly installment (p), rate of interest (r) and time period in months (n).
+Calculate and output the maturity amount (a) receivable using the formula a = p * n +( p * n(n + 1) / 2 * r / 100 * 1 /12.
+For an incorrect option, an appropriate error message should be displayed.
+*/
+
 import java.util.Scanner;
+
 class P2
 {
     public static void main(String args[])
     {
         double p,r,n,a;
+        
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Menu:");
         System.out.println("\n1.Term Deposit");
         System.out.println("2.Recurring Deposit");
