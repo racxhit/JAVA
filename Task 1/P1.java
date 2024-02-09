ac2ff16c5ce6c6261fbfb4e990bb9e81dcd16509
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-09 19:57:27 +0530

    Update P1.java

--- Task 1/P1.java
+++ Task 1/P1.java
@@ -1,15 +1,28 @@
+/*
+Implement a java program to take input as a command line argument. 
+Your name, course, university Rollno and semester. 
+Display the information:
+
+Name: 
+University Roll No: 
+Course:
+Semester:
+*/
+
 class CommandLineArguments
 {
     public static void main(String[] args)
     {
         if (args.length != 5)
         {
-            System.out.println("Please provide four command line arguments: full name, course, university roll number, and semester.");
-            return;
+            System.out.println("Please provide four command line arguments: Full Name, Course, University Roll Number, and Semester.");
         }
+        else
+        {
         System.out.println("Name: " + args[0] + " " + args[1]);
         System.out.println("Course: " + args[2]);
-        System.out.println("University Roll Number: " + args[3]);
+        System.out.println("University Roll No: " + args[3]);
         System.out.println("Semester: " + args[4]);
+        }
     }
 }
